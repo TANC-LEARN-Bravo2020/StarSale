0d956cec5125412ca0f6729bed233d959ff366ec
nbrooks89 <nbrooks2389@gmail.com>
2020-07-06 09:42:40 -0700

    add lat and long columns to schema

---
+++ db/migrate/20200706164003_add_latitude_and_longitude_to_column.rb
@@ -0,0 +1,7 @@
+class AddLatitudeAndLongitudeToColumn < ActiveRecord::Migration[6.0]
+  def change
+    add_column :sales, :latitude, :decimal
+    add_column :sales, :longitude, :decimal
+
+  end
+end
