b917b04b2460a09373c0be3fb15599f9df7dce4e
Chantelle Isaacs <chantelle616@gmail.com>
2020-07-06 13:59:08 -0700

    fixing schema and migrations

--- db/migrate/20200706164003_add_latitude_and_longitude_to_column.rb
+++
@@ -1,7 +0,0 @@
-class AddLatitudeAndLongitudeToColumn < ActiveRecord::Migration[6.0]
-  def change
-    add_column :sales, :latitude, :decimal
-    add_column :sales, :longitude, :decimal
-
-  end
-end
