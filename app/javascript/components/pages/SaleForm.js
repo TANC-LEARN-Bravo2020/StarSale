38383e98e3c6290ac032dfc3130d4e7bffcbb446
Chantelle Isaacs <chantelle616@gmail.com>
2020-07-05 17:48:11 -0700

    add styling to all pages

--- app/javascript/components/pages/SaleForm.js
+++ app/javascript/components/pages/SaleForm.js
@@ -52,55 +52,67 @@ class SaleForm extends React.Component {
   render () {
     return (
       <>
-      <Container>
-        <Form>
-          <FormGroup>
+      <div className="form-page">
+      <Container className="form-container">
+        <h2 className="page-header">Create a New Sale</h2>
+        <Form className="row">
+          <FormGroup className="col-sm">
               <Label for="address">Street Address</Label>
               <Input type="text" name="address" id="address" placeholder="123 Main St" value={ this.state.form.address } onChange={ this.handleChange} />
           </FormGroup>
-          <FormGroup>
-              <Label for="city">City</Label>
-              <Input type="text" name="city" id="city" placeholder="Townsville"value={ this.state.form.city } onChange={ this.handleChange}  />
-          </FormGroup>
-          <FormGroup>
-              <Label for="state">State</Label>
-              <Input type="text" name="state" id="state" placeholder="NY" value={ this.state.form.state } onChange={ this.handleChange} />
-          </FormGroup>
-          <FormGroup>
-              <Label for="zip">Zip Code</Label>
-              <Input type="text" name="zip" id="zip" placeholder="10101" value={ this.state.form.zip } onChange={ this.handleChange}  />
-          </FormGroup>
-          <FormGroup>
-              <Label for="date">Sale Date</Label>
-              <Input type="date" name="date" id="date" placeholder="" value={ this.state.form.date } onChange={ this.handleChange} />
-          </FormGroup>
-          <FormGroup>
-              <Label for="duration">Duration</Label>
-              <Input type="text" name="duration" id="duration" placeholder="9am-5pm" value={ this.state.form.duration } onChange={ this.handleChange} />
-          </FormGroup>
-          <FormGroup>
+          <FormGroup className="col-sm">
               <Label for="title">Sale Name</Label>
               <Input type="text" name="title" id="title" placeholder="Toys and Tools" value={ this.state.form.title } onChange={ this.handleChange} />
           </FormGroup>
-          <FormGroup>
-              <Label for="description">Description</Label>
-              <Input type="textarea" name="description" id="description" placeholder="Tell us about the apartment" value={ this.state.form.description } onChange={ this.handleChange} />
+        </Form>
+        <Form className="row">
+          <FormGroup className="col-sm">
+              <Label for="city">City</Label>
+              <Input type="text" name="city" id="city" placeholder="Townsville"value={ this.state.form.city } onChange={ this.handleChange}  />
           </FormGroup>
-          <FormGroup>
+          <FormGroup className="col-sm">
+              <Label for="description">Description</Label>
+              <Input type="textarea" name="description" id="description" placeholder="Tell us about the items you'll be selling - be as descriptive as possible so people will come by!" value={ this.state.form.description } onChange={ this.handleChange} />
+          </FormGroup>
+        </Form>
+        <Form className="row">
+          <FormGroup className="col-sm">
+              <Label for="state">State</Label>
+              <Input type="text" name="state" id="state" placeholder="NY" value={ this.state.form.state } onChange={ this.handleChange} />
+          </FormGroup>
+          <FormGroup className="col-sm">
+              <Label for="date">Sale Date</Label>
+              <Input type="date" name="date" id="date" placeholder="" value={ this.state.form.date } onChange={ this.handleChange} />
+          </FormGroup>
+        </Form>
+        <Form className="row"> 
+          <FormGroup className="col-sm">
+              <Label for="zip">Zip Code</Label>
+              <Input type="text" name="zip" id="zip" placeholder="10101" value={ this.state.form.zip } onChange={ this.handleChange}  />
+          </FormGroup>
+          <FormGroup className="col-sm">
+              <Label for="duration">Duration</Label>
+              <Input type="text" name="duration" id="duration" placeholder="9am-5pm" value={ this.state.form.duration } onChange={ this.handleChange} />
+          </FormGroup>
+         </Form>
+         <Form className="row"> 
+          
+          <FormGroup className="col-sm">
               <Label for="payment_type">Payment Types Accepted</Label>
               <Input type="text" name="payment_type" id="payment_type" placeholder="Cash and Venmo" value={ this.state.form.payment_type } onChange={ this.handleChange} />
           </FormGroup>
-          <FormGroup>
+          <FormGroup className="col-sm">
               <Label for="img">Picture URL</Label>
               <Input type="url" name="img" id="state" value={ this.state.form.img } onChange={ this.handleChange} />
-              <FormText>Please paste the url of an image of your apartment from an external source website such as Flickr or Imgur</FormText>
+              <FormText>Please paste the url of an image of your items for sale from an external source website such as Flickr or Imgur</FormText>
           </FormGroup>
+          </Form>
           <Button onClick={this.handleSubmit} type="submit">Submit</Button>
           {this.state.success &&
           <Redirect to="/" />
             }
-        </Form>
       </Container>
+      </div>
       </>
     );
   }
