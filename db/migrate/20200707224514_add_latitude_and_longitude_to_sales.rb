5b1e7fd457c6b020da538c89814bb65a295d8ba6
Chantelle Isaacs <chantelle616@gmail.com>
2020-07-07 15:52:47 -0700

    recreated migration for adding latitude and longitude to sales

---
+++ db/migrate/20200707224514_add_latitude_and_longitude_to_sales.rb
@@ -0,0 +1,6 @@
+class AddLatitudeAndLongitudeToSales < ActiveRecord::Migration[6.0]
+  def change
+    add_column :sales, :latitude, :decimal
+    add_column :sales, :longitude, :decimal
+  end
+end
