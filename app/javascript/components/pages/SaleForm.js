d2b28c3e4c3a0c0e7e13681aad2e622ab8bfeda9
Chantelle Isaacs <chantelle616@gmail.com>
2020-07-08 16:39:05 -0700

    add presence validations to sale

--- app/javascript/components/pages/SaleForm.js
+++ app/javascript/components/pages/SaleForm.js
@@ -88,48 +88,48 @@ class SaleForm extends React.Component {
         <h2 className="page-header">Create a New Sale</h2>
         <Form className="row">
           <FormGroup className="col-sm">
-              <Label for="address">Street Address</Label>
+              <Label for="address">Street Address*</Label>
               <Input type="text" name="address" id="address" placeholder="123 Main St" value={ this.state.form.address } onChange={ this.handleChange} />
           </FormGroup>
           <FormGroup className="col-sm">
-              <Label for="title">Sale Name</Label>
+              <Label for="title">Sale Name*</Label>
               <Input type="text" name="title" id="title" placeholder="Toys and Tools" value={ this.state.form.title } onChange={ this.handleChange} />
           </FormGroup>
         </Form>
         <Form className="row">
           <FormGroup className="col-sm">
-              <Label for="city">City</Label>
+              <Label for="city">City*</Label>
               <Input type="text" name="city" id="city" placeholder="Townsville"value={ this.state.form.city } onChange={ this.handleChange}  />
           </FormGroup>
           <FormGroup className="col-sm">
-              <Label for="description">Description</Label>
+              <Label for="description">Description*</Label>
               <Input type="textarea" name="description" id="description" placeholder="Tell us about the items you'll be selling - be as descriptive as possible so people will come by!" value={ this.state.form.description } onChange={ this.handleChange} />
           </FormGroup>
         </Form>
         <Form className="row">
           <FormGroup className="col-sm">
-              <Label for="state">State</Label>
+              <Label for="state">State*</Label>
               <Input type="text" name="state" id="state" placeholder="NY" value={ this.state.form.state } onChange={ this.handleChange} />
           </FormGroup>
           <FormGroup className="col-sm">
-              <Label for="date">Sale Date</Label>
+              <Label for="date">Sale Date*</Label>
               <Input type="date" name="date" id="date" placeholder="" value={ this.state.form.date } onChange={ this.handleChange} />
           </FormGroup>
         </Form>
         <Form className="row">
           <FormGroup className="col-sm">
-              <Label for="zip">Zip Code</Label>
+              <Label for="zip">Zip Code*</Label>
               <Input type="text" name="zip" id="zip" placeholder="10101" value={ this.state.form.zip } onChange={ this.handleChange}  />
           </FormGroup>
           <FormGroup className="col-sm">
-              <Label for="duration">Duration</Label>
+              <Label for="duration">Duration*</Label>
               <Input type="text" name="duration" id="duration" placeholder="9am-5pm" value={ this.state.form.duration } onChange={ this.handleChange} />
           </FormGroup>
          </Form>
          <Form className="row">
 
           <FormGroup className="col-sm">
-              <Label for="payment_type">Payment Types Accepted</Label>
+              <Label for="payment_type">Payment Types Accepted*</Label>
               <Input type="text" name="payment_type" id="payment_type" placeholder="Cash and Venmo" value={ this.state.form.payment_type } onChange={ this.handleChange} />
           </FormGroup>
           <FormGroup className="col-sm">
@@ -138,6 +138,8 @@ class SaleForm extends React.Component {
               <FormText>Please paste the url of an image of your items for sale from an external source website such as Flickr or Imgur</FormText>
           </FormGroup>
           </Form>
+          <FormText>* indicates a required field.</FormText>
+
           <Button onClick={this.handleSubmit} type="submit">Submit</Button>
           {this.state.success &&
           <Redirect to="/" />
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,4 @@
 class Sale < ApplicationRecord
     belongs_to :user
+    validates :address, :city, :state, :zip, :date, :duration, :title, :description, :payment_type, presence: true
 end
