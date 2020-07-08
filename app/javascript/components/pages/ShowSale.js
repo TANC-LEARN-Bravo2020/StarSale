d7bae2ff74d7d8d3ef5431f94be17e4a6ae38107
masanes <aaronmasanes@hotmail.com>
2020-07-08 11:45:17 -0700

    fixed error with signed out error not being able to see sales

--- app/javascript/components/pages/ShowSale.js
+++ app/javascript/components/pages/ShowSale.js
@@ -17,50 +17,55 @@ class ShowSale extends React.Component {
       sale: [],
       deletesuccess: false,
       update: false,
-      favorited:false,
-      favId:""
+      favorited: false,
+      favId: "",
     };
     this.getSale();
   }
   componentDidMount() {
     this.getSale();
   }
-  async getSale (props){
+  async getSale(props) {
     const { id } = this.props.match.params;
+
     try {
-      // Fetch JSON of favorites specific to current user
-      let favResponse = await fetch("/favorite")
-      let favData = await favResponse.json()
-      // Declare array to hold only favorited apt ids to be used in both if-statements below
-      let favAptIdsArray
-      if(favResponse.ok) {
-        console.log("favData:", favData)
-        favData.map(favorite=> {
-          // Determine the favorite id (for use in favorite delete call) if current apt is currently favorited
-          if (favorite.sale_id == this.props.match.params.id) {
-            this.setState({favId:favorite.id})
+      if (this.props.current_user.id !== 0) {
+        // Fetch JSON of favorites specific to current user
+        let favResponse = await fetch("/favorite");
+        let favData = await favResponse.json();
+        // Declare array to hold only favorited apt ids to be used in both if-statements below
+        let favAptIdsArray;
+        if (favResponse.ok) {
+          console.log("favData:", favData);
+          favData.map((favorite) => {
+            // Determine the favorite id (for use in favorite delete call) if current apt is currently favorited
+            if (favorite.sale_id == this.props.match.params.id) {
+              this.setState({ favId: favorite.id });
+            }
+          });
+          // Create array of just the ids of the apts favorited by current user
+          favAptIdsArray = favData.map((value) => value.sale_id);
+          console.log("favAptIdsArray:", favAptIdsArray);
+        }
+      }
+
+      fetch(`/sales/${id}`)
+        .then((response) => {
+          if (response.status === 200) {
+            return response.json();
           }
         })
-        // Create array of just the ids of the apts favorited by current user
-        favAptIdsArray = favData.map(value=>value.sale_id)
-        console.log("favAptIdsArray:",favAptIdsArray)
-      }
-    
-    fetch(`/sales/${id}`)
-      .then((response) => {
-        if (response.status === 200) {
-          return response.json();
-        }
-      })
-      .then((saleJSON) => {
-        this.setState({ sale: saleJSON });
-        if (favAptIdsArray.includes(saleJSON.id)) {
-          this.setState({favorited:true})}
-      });
-  }catch (err) {
-    console.log(err)
+        .then((saleJSON) => {
+          console.log("test saleJSon", saleJSON);
+          this.setState({ sale: saleJSON });
+          if (favAptIdsArray.includes(saleJSON.id)) {
+            this.setState({ favorited: true });
+          }
+        });
+    } catch (err) {
+      console.log(err);
+    }
   }
-};
 
   deleteSale = (props) => {
     const { id } = this.props.match.params;
@@ -77,61 +82,62 @@ class ShowSale extends React.Component {
       })
       .then(() => {
         this.setState({ deletesuccess: true });
-      }); 
+      });
   };
 
   addToFavorites = () => {
-    console.log(this.props.match.params.id, this.props.current_user.id)
-    let fave = {sale_id: this.props.match.params.id, user_id:this.props.current_user.id}
+    console.log(this.props.match.params.id, this.props.current_user.id);
+    let fave = {
+      sale_id: this.props.match.params.id,
+      user_id: this.props.current_user.id,
+    };
     fetch("/favorite", {
-      body: JSON.stringify({favorite: fave}),
-      headers:{
-        "Content-Type": "application/json"
+      body: JSON.stringify({ favorite: fave }),
+      headers: {
+        "Content-Type": "application/json",
       },
-      method: "POST"
+      method: "POST",
     })
-    .then(response => {
-      if (response.ok) {
-        // If favorite post request is successful, set favorited to true
-        this.setState({favorited: true})
-      }
-    })
-    .then(() => {
-      // Call the sale API call again to update frontend data
-      this.getSale()
-    })
-  }
+      .then((response) => {
+        if (response.ok) {
+          // If favorite post request is successful, set favorited to true
+          this.setState({ favorited: true });
+        }
+      })
+      .then(() => {
+        // Call the sale API call again to update frontend data
+        this.getSale();
+      });
+  };
 
   // Delete apt id from favorited from Favorite model
   removeFromFavorites = () => {
     fetch(`/favorite/${this.state.favId}`, {
-      headers:{
-        "Content-Type": "application/json"
+      headers: {
+        "Content-Type": "application/json",
       },
-      method: "DELETE"
+      method: "DELETE",
     })
-    .then(response => {
-      if (response.ok) {
-        // If favorite delete request is successful, set favorited to false
-        this.setState({favorited: false})
-      }
-    })
-    .then(() => {
-      // Call the apartment API call again to update frontend data
-      this.getSale()
-    })
-  }
+      .then((response) => {
+        if (response.ok) {
+          // If favorite delete request is successful, set favorited to false
+          this.setState({ favorited: false });
+        }
+      })
+      .then(() => {
+        // Call the apartment API call again to update frontend data
+        this.getSale();
+      });
+  };
 
   // On click for the follow/following button
   handleFavorite = (e) => {
-    e.preventDefault()
-    console.log(this.props.current_user.id, this.state.sale.id)
-    if (this.state.favorited === false){
-      this.addToFavorites()
-    } 
-    else this.removeFromFavorites()
-  }
-
+    e.preventDefault();
+    console.log(this.props.current_user.id, this.state.sale.id);
+    if (this.state.favorited === false) {
+      this.addToFavorites();
+    } else this.removeFromFavorites();
+  };
 
   updateRedirect = () => {
     this.setState({ update: true });
@@ -144,30 +150,32 @@ class ShowSale extends React.Component {
       <>
         <Container className="form-container">
           <div className="row">
-          <div className="col-6">
-            <img src={sale.img} className="sale-img" />
-            <button onClick={(e)=>this.handleFavorite(e)}>Fave!</button>
-            {this.state.favorited && <p>FAVED! IT WORKS</p>}
-          </div>
-          <div className="col-6">
-            <h2>{sale.title}</h2>
-            <h4>Address: {sale.address},</h4>
-            <h4>
-              {" "}
-              {sale.city}, {sale.state} {sale.zip}
-            </h4>
-            <h4>Payment Type: {sale.payment_type}</h4>
-            <p>{sale.description}</p>
-            {console.log(current_user, sale.user_id)}
-            {current_user.id === sale.user_id && (
-              <div>
-                <button onClick={this.deleteSale}>Delete this Sale</button>
-                {this.state.deletesuccess && <Redirect to="/" />}
-                <button onClick={this.updateRedirect}>Update Sale</button>
-                {this.state.update && <Redirect to={`/saleupdate/${sale.id}`} />}
-              </div>
-            )}
-          </div>
+            <div className="col-6">
+              <img src={sale.img} className="sale-img" />
+              <button onClick={(e) => this.handleFavorite(e)}>Fave!</button>
+              {this.state.favorited && <p>FAVED! IT WORKS</p>}
+            </div>
+            <div className="col-6">
+              <h2>{sale.title}</h2>
+              <h4>Address: {sale.address},</h4>
+              <h4>
+                {" "}
+                {sale.city}, {sale.state} {sale.zip}
+              </h4>
+              <h4>Payment Type: {sale.payment_type}</h4>
+              <p>{sale.description}</p>
+              {console.log(current_user, sale.user_id)}
+              {current_user.id === sale.user_id && (
+                <div>
+                  <button onClick={this.deleteSale}>Delete this Sale</button>
+                  {this.state.deletesuccess && <Redirect to="/" />}
+                  <button onClick={this.updateRedirect}>Update Sale</button>
+                  {this.state.update && (
+                    <Redirect to={`/saleupdate/${sale.id}`} />
+                  )}
+                </div>
+              )}
+            </div>
           </div>
         </Container>
       </>
