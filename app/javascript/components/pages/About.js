942cd7ece7110bf3a27ed47fd309b91bd7122b0b
Thomas Gonda <passinto06@gmail.com>
2020-07-13 10:06:03 -0700

    about page changes

--- app/javascript/components/pages/About.js
+++ app/javascript/components/pages/About.js
@@ -4,6 +4,8 @@ import nikki from "../nikki.jpeg"
 import thomas from "../thomas.jpeg"
 import chantelle from "../chantelle.jpeg"
 import groupPhoto from "../StarSaleGroup.jpg"
+import githubLogo from "../github.png"
+import linkedinLogo from "../linkedin.png"
 import {Container } from 'reactstrap'
 
 class About extends React.Component {
@@ -12,19 +14,27 @@ class About extends React.Component {
     this.state = {
       info: [{
         imageURL: thomas,
-        about: "I am originally from Oakland California but for the past few years I have been attending San Diego State down in Socal. I enjoy working with front-end applications because it gives me an opportunity to utilize coding to create experiences that let me connect with other people. My hobbies are Chess, Soccer, and competitive video games."
+        about: "Hello, My name is Thomas Gonda.  I am originally from Oakland, California but for the past few years I have been attending San Diego State down in Socal. I enjoy working with front-end applications because it gives me an opportunity to utilize coding to create experiences that let me connect with other people. My hobbies are Chess, Soccer, and competitive video games.",
+        linkedin: "https://www.linkedin.com/in/thomasgonda3/",
+        github: "https://github.com/thomasgonda3"
       },
       {
         imageURL: aaron,
-        about: "I'm a Full-Stack web-developer at LEARN academy and graduated with my BA in Information system management. I am currently working with languages on the front-end like Javascript, HTML, CSS and ruby."
+        about: "My name is Aaron Masanes, and I am a Full-Stack web-developer at LEARN academy and graduated with my BA in Information system management. I am currently working with languages on the front-end like Javascript, HTML, CSS and ruby.",
+        linkedin: "https://www.linkedin.com/in/aaron-masanes/",
+        github: "https://github.com/masanes"
       },
       {
         imageURL: nikki,
-        about: "Hi there! My name is Nikki Brooks. I am a recent graduate of a full stack web development bootcamp, eager to utilize my new skills. I am passionate about building amazing websites that influence and improve the lives of those around me. When im not coding i enjoy, skateboarding, and hanging out at the beach with my dog Archie."
+        about: "Hi there! My name is Nikki Brooks. I am a recent graduate of a full stack web development bootcamp, eager to utilize my new skills. I am passionate about building amazing websites that influence and improve the lives of those around me. When im not coding i enjoy, skateboarding, and hanging out at the beach with my dog Archie.",
+        linkedin: "https://www.linkedin.com/in/nicole-brooks89/",
+        github: "https://github.com/nbrooks89"
       },
       {
         imageURL: chantelle,
-        about: "A move to San Diego gave me the opportunity to attend the (appropriately named) LEARN Academy where my world of learning has been expanded. Becoming a Full Stack Web Developer has allowed me to open the hood of the digital world we live in and see the engine inside - and it’s fascinating. I’d love to learn about your ideas, your projects, and how we can work together to make them a reality."
+        about: "A move to San Diego gave me the opportunity to attend the (appropriately named) LEARN Academy where my world of learning has been expanded. Becoming a Full Stack Web Developer has allowed me to open the hood of the digital world we live in and see the engine inside - and it’s fascinating. I’d love to learn about your ideas, your projects, and how we can work together to make them a reality.",
+        linkedin: "https://www.linkedin.com/in/chantellerisaacs/",
+        github: "https://github.com/chantelle-isaacs"
       }]
     };
   }
@@ -41,7 +51,14 @@ class About extends React.Component {
             <div className="column-middle">
               <p className="about-text">{value.about}</p>
             </div>
-            <div className="column-right"></div>
+            <div className="column-right">
+              <a href={value.linkedin}>
+                <img className="developer-links" src={linkedinLogo} width="70px" height="70px"></img>
+              </a>
+              <a href={value.github}>
+                <img className="developer-links" src={githubLogo} width="70px" height="70px"></img>
+              </a>
+            </div>
           </div>
           {index !== 3 &&
           <hr className="profile-border"></hr>
